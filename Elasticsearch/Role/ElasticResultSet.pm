ee93d1aa0abe024c786fd54034010e3b7845d39b
Patrick Simon <pas.freelance@googlemail.com>
2015-09-22 10:03:23 +0200

    role inheriti

--- Elasticsearch/Role/ElasticResultSet.pm
+++ Elasticsearch/Role/ElasticResultSet.pm
@@ -3,6 +3,6 @@ package OASYS::ElasticResultSet;
 use strict;
 use warnings;
 
-use base qw/DBIx::Class::Elasticsearch::ResultSet DBIx::Class::ResultSet/;
+extends 'DBIx::Class::Elasticsearch::ResultSet', 'DBIx::Class::ResultSet';
 
 1;
