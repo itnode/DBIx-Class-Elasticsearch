0e1f6cae299d740de60ee62f1fe696fdbb7d39e9
Patrick Simon <pas.freelance@googlemail.com>
2015-09-22 09:31:14 +0200

    created roles refs #1410

---
+++ Elasticsearch/Role/ElasticResult.pm
@@ -0,0 +1,8 @@
+package OASYS::ElasticResult;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Elasticsearch::Result DBIx::Class::Core/;
+
+1;
---
+++ Elasticsearch/Role/ElasticResultSet.pm
@@ -0,0 +1,8 @@
+package OASYS::ElasticResultSet;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Elasticsearch::ResultSet DBIx::Class::ResultSet/;
+
+1;
